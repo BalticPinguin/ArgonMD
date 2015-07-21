57418384831011e294c1e0152f20eb58a72d679f
Tobias Moehle <tobias.moehle@uni-rostock.de>
2015-07-21 11:52:31 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import physics as ph
+
+def frange(start, stop, step): #imitate range, but with floats.
+   r = start
+   i=0
+   while r <= stop:
+      yield r
+      i+=1
+      r =start + i*step
+
+def main():
+   N=50 #number of particles
+   #length in angstroem, integer required
+   L=10 #size of box; needs to be cubic for the method to be working
+   T=200
+   #time-step (in ps)
+   dt=0.50
+   t=10e3
+   output="box.dat"
+   #now, start simulation
+
+   particle,mass=ph.seed(N,L, T)
+   force=ph.update_force(particle) #get forces
+   ph.print_conf(particle,output, 0)
+   for time in frange(dt,t,dt):
+      force,particle=ph.propagate(force,particle,L, dt,mass)
+      ph.print_conf(particle,output, time)
+
+if __name__=="__main__":
+   main()
---
+++ physics.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+import numpy as np
+import random as rand
+
+def seed(N,L,T):
+   """ simple method for placing particles randomly in the cell. No test of overlapping (small distances) is done.
+   """
+   k=2.07e-8
+   phasecoord=np.zeros((N,6))
+   mass=np.ones(N) #can be changed to treat different particles
+   for particle in range(N):
+      for coord in range(3):
+         phasecoord[particle][coord]= rand.random()*L #random float in the box
+      for coord in range(3,6):
+         # allow for velocities between 1 and -1
+         phasecoord[particle][coord]=(rand.random()-0.5)*2*np.sqrt(T*3*k/mass[particle])/np.sqrt(3)
+   return phasecoord,mass
+
+def propagate(f,particle,L,dt, mass):
+   """ Calculate one propagation step 
+   """
+   #update force and momentum for particles:
+   numPart=len(particle)
+   for part in range(numPart):
+      #print( particle[part][3:],1/(2*mass[part])*f[part])
+      particle[part][:3]+=dt*particle[part][3:]+dt*dt/(2*mass[part])*f[part]
+      particle[part][:3]+=dt/mass[part]*f[part]/2
+      #print(f[part])
+      for i in range(3):
+         x=particle[part][i]
+         particle[part][i]=particle[part][i]%L
+       #  if abs(x-particle[part][i])>2:
+       #     print(x, particle[part][i])
+   f=update_force(particle)
+   for part in range(numPart):
+      particle[part][:3]+=dt/mass[part]*f[part]/2
+   return f,particle
+
+def update_force(particle):
+   def F(r):
+      """returns the F/r for the Lennard-Jones potential
+      """
+      k_b=2.07e-8
+      epsilon=1e-10
+      sigma=3.4
+      epsilon=120.*k_b #in A/ps
+      if r>2.5*sigma:
+         return 0
+      if r<epsilon:
+         return 4
+      x=pow(sigma/r,6.)
+      print(24*epsilon*(x-2*x*x)/(r*r))
+      return 24*epsilon*(2*x-1)*x/(r*r)
+   
+   numPart=len(particle)
+   f=np.zeros((numPart,3))
+   r_ij=np.zeros(3)
+   f_ij=np.zeros(3)
+   for part in range(numPart):
+      f_ij*=0 #set the vector to 0
+      for interact in range(numPart):
+         if part!=interact: #avoid self-interaction
+            for i in range(3):
+               r_ij[i]=particle[part][i]-particle[interact][i]
+            f_ij+=F(r_ij[0]*r_ij[0]+r_ij[1]*r_ij[1]+r_ij[2]*r_ij[2])*r_ij
+         f[part]=f_ij
+   return f
+
+def temp(particle):
+   N=len(particle)
+   k_b=2.07e-8
+   T=0
+   for i in range(N):
+      for j in range(3,6):
+         T+=particle[i][j]*particle[i][j]
+   T/=3*N*k_b
+   return T
+
+def print_conf(particle,output, t):
+   out=open(output,"a")
+   T=temp(particle)
+   for i in range(len(particle)):
+      out.write("%f  %d  %2.6f  %2.6f  %2.6f  %4.6g  %4.6g  %4.6g   %4.8g\n"
+            %(t, i, particle[i][0],  particle[i][1], particle[i][2],particle[i][3],  particle[i][4], particle[i][5],
+            T))
+   out.write("\n\n")
